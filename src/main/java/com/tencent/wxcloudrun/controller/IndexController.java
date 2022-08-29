3ce6a86458d369df2ffaeb5074bbd89f9a28805b
aaron <372498127@qq.com>
2022-08-29 11:59:07 +0800

    aaa

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tencent.wxcloudrun.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * index控制器
@@ -17,6 +18,12 @@ public class IndexController {
   @GetMapping
   public String index() {
     return "index";
+  }
+
+  @ResponseBody
+  @GetMapping("/welcome")
+  public String welcome(){
+    return "welcome";
   }
 
 }
